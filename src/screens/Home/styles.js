02c624ca68e83a21d1f19ab721a1c176b218f31c
unknown <carlosfreevg@gmail.com>
2021-02-24 22:06:27 -0400

    PageSearch

--- src/screens/Home/styles.js
+++ src/screens/Home/styles.js
@@ -6,12 +6,51 @@ export const Container = styled.SafeAreaView`
   background-color: #63C2D1;
 `;
 
-export const Scroller = styled.ScrollView``;
+export const Scroller = styled.ScrollView`
+  flex: 1;
+  padding: 20px;
+`;
   
-export const HeaderArea = styled.View``;
-export const HeaderTitle = styled.Text``;
-export const SearchButton = styled.TouchableOpacity``;
+export const HeaderArea = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+`;
+export const HeaderTitle = styled.Text`
+  margin-left: 15px;
+  width: 250px;
+  font-size: 18px;
+  font-weight: bold;
+  color: #FFFFFF;
+`;
+export const SearchButton = styled.TouchableOpacity`
+  width: 40px;
+  height: 40px;
+`;
 
-export const LocationArea = styled.View``;
-export const LocationInput = styled.TextInput``;
-export const LocationFinder = styled.TouchableOpacity``; 
+export const LocationArea = styled.View`
+  background-color: #4EADBE;
+  height: 60px;
+  border-radius: 30px;
+  flex-direction: row;
+  align-items: center;
+  padding-left: 20px;
+  padding-right: 20px;
+  margin-top: 30px;
+`;
+export const LocationInput = styled.TextInput`
+  flex: 1;
+  font-size: 14px;
+  color: #FFFFFF;
+`;
+export const LocationFinder = styled.TouchableOpacity`
+  width: 28px;
+  height: 28px;
+`; 
+export const LoadingIcon = styled.ActivityIndicator`
+  margin-top: 50px;
+`;
+export const ListArea = styled.View`
+  margin-top: 30px;
+  margin-bottom: 30px;
+`;
