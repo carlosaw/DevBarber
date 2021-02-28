1429684f22236fe26fa95f3e0369e5e85a98aaf2
unknown <carlosfreevg@gmail.com>
2021-02-28 17:39:40 -0400

    TelaProfile

--- src/screens/Profile/index.js
+++ src/screens/Profile/index.js
@@ -1,12 +1,31 @@
-import React from 'react';
-import { Text, Button } from 'react-native';
-import { useNavigation } from '@react-navigation/native';
-import { Container } from './styles';
+import React, { useState } from 'react';
+import { useNavigation, useRoute } from '@react-navigation/native';
+import AsyncStorage from '@react-native-community/async-storage';
+//import { UserContext } from '../../contexts/UserContext';
+import {
+  Container,
+  InputArea,
+  CustomButton,
+  CustomButtonText,
+} from './styles';
+
+import SignInput from '../../components/SignInput';
 
 import Api from '../../Api';
 
+import BarberLogo from '../../assets/barber.svg';
+
+import PersonIcon from '../../assets/person.svg';
+import EmailIcon from '../../assets/email.svg';
+import LockIcon from '../../assets/lock.svg';
+
 export default () => {
   const navigation = useNavigation();
+
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [password_confirmation, setPasswordConfirmation] = useState('');
 
   const handleLogoutClick = async () => {
     await Api.logout();
@@ -15,10 +34,49 @@ export default () => {
     });
   }
 
-  return (
+  const handleUpdateClick = async () => {
+    
+  }
+
+  return(
     <Container>
-      <Text>Tela PROFILE</Text>
-      <Button title="Sair" onPress={handleLogoutClick} />
+      <BarberLogo width="100%" height="160" />
+
+      <InputArea>
+
+        <SignInput
+          IconSvg={PersonIcon}
+          value={name}
+          onChangeText={t=>setName(t)}
+          onFocus={true}
+        />        
+        <SignInput
+          IconSvg={EmailIcon}
+          value={email}
+          onChangeText={t=>setEmail(t)}
+        />
+        <SignInput
+          IconSvg={LockIcon}
+          value={password}
+          onChangeText={t=>setPassword(t)}
+          password={true}
+        />
+        <SignInput
+          IconSvg={LockIcon}
+          value={password_confirmation}
+          onChangeText={t=>setPasswordConfirmation(t)}
+          password={true}
+        />
+
+        <CustomButton onPress={handleUpdateClick}>
+          <CustomButtonText>Atualizar Cadastro</CustomButtonText>
+        </CustomButton>
+
+        <CustomButton onPress={handleLogoutClick}>
+          <CustomButtonText>Sair</CustomButtonText>
+        </CustomButton>
+        
+      </InputArea>
     </Container>
   );
 }
--- src/screens/Profile/styles.js
+++ src/screens/Profile/styles.js
@@ -1,7 +1,26 @@
 import React from 'react';
 import styled from 'styled-components/native';
-
+ 
 export const Container = styled.SafeAreaView`
+  background-color: #63C2D1;
   flex: 1;
-  background-color: #4EADBE;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const InputArea = styled.View`
+  width: 100%;
+  padding: 40px;
+`;
+export const CustomButton = styled.TouchableOpacity`
+  height: 50px;
+  background-color: #268596;
+  border-radius: 25px;
+  justify-content: center;
+  align-items: center;
+  margin-top: 30px;
+`;
+export const CustomButtonText = styled.Text`
+  font-size: 14px;
+  color: #FFF;
 `;
